7e4f48d44edd5fa03d02f68ec130f446b37205df
t-kugai <t-kugai@connectill.jp>
2021-12-24 10:51:34 +0900

    リンク先変更、ページネーション変更、JS追加

--- js/custom.js
+++ js/custom.js
@@ -11,3 +11,16 @@ btn_passview.addEventListener('click', (e) => {
     pwd_check.setAttribute("src", "images/icon05.png");
   }
 });
+
+const btn_passview2 = document.getElementById("passwordImg2");
+btn_passview2.addEventListener('click', (e) => {
+  let input_type = document.getElementById("passWordArea2");
+  let pwd_check = document.getElementById("passwordImg2")
+  if (input_type.type === "password") {
+    input_type.type = "text";
+    pwd_check.setAttribute("src", "images/icon04.png");
+  } else {
+    input_type.setAttribute("type", "password");
+    pwd_check.setAttribute("src", "images/icon05.png");
+  }
+});
